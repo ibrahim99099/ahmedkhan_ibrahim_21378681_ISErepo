9bb498324cff31f44fc8d924d273bd2c64d5d603
Ibrahim <ibrahimk_786@hotmail.com>
2023-03-30 19:19:57 +0400

    called the above fibonacci function to prevent redundancy

---
+++ closefibonaccitest.py
@@ -0,0 +1,50 @@
+import sys
+import io
+import maths_fun
+
+
+def setUp():
+    capOut = io.StringIO()
+    sys.stdout = capOut
+    return capOut
+
+def testfibonacci1():
+    capOut = setUp()
+    maths_fun.closefibonacci(5)
+    assert ("5 is in the Fibonacci sequence") == capOut.getvalue().strip()
+    tearDown()
+
+def testfibonacci2():
+    capOut = setUp()
+    maths_fun.closefibonacci(9)
+    assert ("9 is not in the Fibonacci sequence\nThe next larger number in the Fibonacci sequence is 13") == capOut.getvalue().strip()
+    tearDown()
+
+def testfibonacci3():
+    capOut = setUp()
+    maths_fun.fibonacci(0)
+    assert ("0 is in the Fibonacci sequence") == capOut.getvalue().strip()
+    tearDown()
+
+def testfibonacci4():
+    capOut = setUp()
+    maths_fun.fibonacci(-3)
+    assert ("-3 is not in the Fibonacci sequence (negative numbers are not in the sequence)") == capOut.getvalue().strip()
+    tearDown()
+
+def tearDown():
+    sys.stdout = sys.__stdout__
+
+if __name__ == "__main__":
+    setUp()
+    testfibonacci1()
+    tearDown()
+    setUp()
+    testfibonacci2()
+    tearDown()
+    setUp()
+    testfibonacci3()
+    tearDown()
+    setUp()
+    testfibonacci4()
+    tearDown()
