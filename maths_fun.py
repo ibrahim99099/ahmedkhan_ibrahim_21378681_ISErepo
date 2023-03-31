8b9cbde51ea272cf5a67a44637ba801d6c5b031e
Ibrahim <ibrahimk_786@hotmail.com>
2023-03-31 14:26:46 +0400

    doing the 4th method using file input so that it reads from the txt file

--- maths_fun.py
+++ maths_fun.py
@@ -27,6 +27,28 @@ def is_integer(n):
     except ValueError:
         return False
 
+def check_fibonacci_numbers(filename):
+    """Reads a file containing a series of numbers and checks which ones are in the Fibonacci sequence"""
+    fib_numbers = []
+    non_fib_numbers = []
+    try:
+        with open(filename) as file:
+            numbers = file.read().split()
+            for num in numbers:
+                num = int(num)
+                if num < 0:
+                    non_fib_numbers.append(num)
+                else:
+                    a, b = 0, 1
+                    while b < num:
+                        a, b = b, a + b
+                    if b == num:
+                        fib_numbers.append(num)
+                    else:
+                        non_fib_numbers.append(num)
+    except OSError:
+        print(f"Error reading file {filename}")
+    return fib_numbers, non_fib_numbers
 
 
 
